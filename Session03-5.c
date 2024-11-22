a28c7263c42783823ce4c00428c320397ce37893
TranQuoc2k6 <qatran2k6@gmail.com>
2024-11-22 09:02:03 +0700

    Upload session03-5

---
+++ Session03-5.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+int main(){
+	char hoVaTen1[50] = "Nguyen Van A";
+	int age1 = 20 ;
+	char phone1[50] = "0904488481";
+	char email1[50] = "anv@rikkeiacademy.com" ;
+	
+	char hoVaTen2[50] = "Nguyen Van B";
+	int age2 = 21 ;
+	char phone2[50] = "0904488482";
+	char email2[50] = "bnv@rikkeiacademy.com" ;
+	
+	char hoVaTen3[50] = "Nguyen Van C";
+	int age3 = 18 ;
+	char phone3[50] = "0904488483";
+	char email3[50] = "cnv@rikkeiacademy.com" ;
+	
+	char hoVaTen4[50] = "Nguyen Van D";
+	int age4 = 19 ;
+	char phone4[50] = "0904488484";
+	char email4[50] = "dnv@rikkeiacademy.com" ;
+	
+	char hoVaTen5[50] = "Nguyen Van E";
+	int age5 = 22 ;
+	char phone5[50] = "0904488485";
+	char email5[50] = "env@rikkeiacademy.com" ;
+	
+	char hoVaTen6[50] = "Nguyen Van F";
+	int age6 = 21 ;
+	char phone6[50] = "0904488486";
+	char email6[50] = "fnv@rikkeiacademy.com" ;
+	
+	char hoVaTen7[50] = "Nguyen Van G";
+	int age7 = 23 ;
+	char phone7[50] = "0904488487";
+	char email7[50] = "gnv@rikkeiacademy.com" ;
+	
+	char hoVaTen8[50] = "Nguyen Van H";
+	int age8 = 19 ;
+	char phone8[50] = "0904488488";
+	char email8[50] = "hnv@rikkeiacademy.com" ;
+	
+	
+    char hoVaTen9[50] = "Nguyen Van I";
+	int age9 = 18 ;
+	char phone9[50] = "0904488489";
+	char email9[50] = "inv@rikkeiacademy.com" ;
+	
+	char hoVaTen10[50] = "Nguyen Van K";
+	int age10 = 21 ;
+	char phone10[50] = "0904488480";
+	char email10[50] = "knv@rikkeiacademy.com" ;
+	
+	
+	printf("\t\t\tDANH SACH SINH VIEN\n");
+	printf("| STT | Ho va ten       | Tuoi  | So dien thoai     | email                       |\n");
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 1   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen1, age1, phone1, email1);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 2   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen2, age2, phone2, email2);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 3   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen3, age3, phone3, email3);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 4   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen4, age4, phone4, email4);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 5   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen5, age5, phone5, email5);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 6   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen6, age6, phone6, email6);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 7   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen7, age7, phone7, email7);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 8   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen8, age8, phone8, email8);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 9   | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen9, age9, phone9, email9);
+	printf("|-----|-----------------|-------|-------------------|-----------------------------|\n");
+	printf("| 10  | %-15s |  %d   |   %-13s   |  %-26s |\n", hoVaTen10, age10, phone10, email10);
+	return 0;
+}
